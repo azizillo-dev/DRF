825d7878305b921ffcecbd85ad69944a81325873
Azizillo Nabiyev Ulug'bek o'g'li <azizillonabiyev52@gmail.com>
2026-08-09 17:14:07 +0500

    Delete Category model from models.py
    Removed the Category model and its related methods.

--- lesson1/vazifa/products/models.py
+++ lesson1/vazifa/products/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 
-
-class Category(models.Model):
-    title = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.title
-
-
-
 class Product(models.Model):
     title = models.CharField(max_length=120)
     price = models.DecimalField(decimal_places=2, max_digits=10)
@@ -21,15 +12,6 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['-id']
-
-
-    
-
-
-
-
-
-
 
 
 
