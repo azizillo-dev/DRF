858c9dfc1b6ebf4567a078c4064c74b7bd3e4ffc
Azizillo <azizillonabiyev52@gmail.com>
2026-08-27 13:51:49 +0500

    auth

--- auth/accounts/serializers.py
+++ auth/accounts/serializers.py
@@ -1,25 +1,62 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from .models import CustomUser
+from rest_framework.exceptions import ValidationError
 
 
 class SignUpSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     id = serializers.CharField(read_only=True)
+    conf_pass = serializers.CharField(write_only=True)
     class Meta:
         model = CustomUser
-        fields = ['id', 'first_name', 'last_name', 'username', 'phone_number', 'address', 'password']
+        fields = ['id', 'first_name', 'last_name', 'username', 'phone_number', 'address', 'password', 'conf_pass']
 
-        def create(self, validated_data):
-            password = validated_data.pop('password')
 
-            user = CustomUser.objects.create_user(
-                password=password,
-                **validated_data
-            )
+    def validate(self, data):
+        password = data.get('password')
+        conf_pass = data.get('conf_pass')
 
-            return user
+        if password and conf_pass and password != conf_pass:
+            raise ValidationError({"msg": "Parollar mos emas"})
+        return data
+
+
+    def  validate_username(self, username):
+        if username[0].isdigit():
+            raise ValidationError({"msg": 'Username raqam bilna boshlanmasin'})
+
 
         
+        return username
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'first_name', 'last_name', 'username', 'phone_number', 'address']
+
+
+
+class ProfileUpdateSerializer(ProfileSerializer):
+    id = serializers.CharField(read_only = True)
+
+
+class PasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField()
+    new_password = serializers.CharField()
+    conf_password = serializers.CharField()
+
+    def validate(self, attrs):
+        old_password = attrs.get('old_password')
+        new_password = attrs.get('new_password')
+        conf_password = attrs.get('conf_password')
+
+        if conf_password and new_password and conf_password != new_password:
+            raise ValidationError(detail='Yangi oarollar mos emas')
+
+        if new_password and old_password and new_password == old_password:
+            raise ValidationError(detail='Yangi parol eskisiga teng bolmasin')
+
+        return attrs
+    
 
--- auth/accounts/views.py
+++ auth/accounts/views.py
@@ -1,18 +1,95 @@
 from django.shortcuts import render
-from .serializers import SignUpSerializer,CustomUser
+from .serializers import SignUpSerializer,ProfileSerializer, ProfileUpdateSerializer, PasswordChangeSerializer
+from .models import CustomUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 
 class SignUpView(APIView):
     def post(self, request):
         serializer = SignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
+        serializer.validated_data.pop('conf_pass')
+        user = CustomUser.objects.create_user(**serializer.validated_data)
+
+        return Response(SignUpSerializer(user).data)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise ValidationError(detail='Parol yoki username xato')
+
+        
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'msg':'Login',
+            'token': token.key
+        }, status=status.HTTP_200_OK)
+    
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated, ]
+    def get(self, request):
+        user = request.user
+        serializer = ProfileSerializer(user)
+        return Response({'msg': 'me', 'data': serializer.data}, status=status.HTTP_200_OK)
+
+    
+
+class ProfileUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+    def patch(self, request):
+        serializer = ProfileUpdateSerializer(instance=request.user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
 
-        return Response(serializer.data)
+        return Response({
+            'msg': 'Profile updated',
+            'data': serializer.data,
+        }, status=status.HTTP_200_OK)
 
 
 
+class PassChangeView(APIView):
+    permission_classes = [IsAuthenticated, ]
+    def put(self, request):
+        user = request.user
+        serializer = PasswordChangeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        current_user = authenticate(username=user.username, password=serializer.validated_data['old_password'])
+        if current_user is None:
+            raise ValidationError(detail='eski parol xato')
 
+        current_user.set_password(serializer.validated_data['new_password'])
+        current_user.save()
+
+        return Response({
+            'msg':'Password updated',
+            
+        }, status=status.HTTP_200_OK)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated,]
+    def post(self, request):
+        Token.objects.filter(user=request.user).delete()
+
+        return Response({
+            'msg': 'Tizimdan chiqdingiz'
+        }, status=status.HTTP_200_OK)
+
+    
